935ecf70e885b6f6d35578e24d45131fb35d7f73
Amires1998 <Amir.malek.esfandiari@gmail.com>
2022-07-19 10:58:30 +0430

    base structure for graphQL Backend structure

---
+++ index.js
@@ -0,0 +1,14 @@
+import {port} from './config/environment' 
+import app from './app'
+
+const start  = async() => {
+    try {
+        await app.listen(port) ; 
+        console.log(`🚀  GraphQL server running at port: ${port}`);
+
+    }catch {
+        console.log('Not able to run GraphQL server');
+    }
+}
+
+start() ; 
